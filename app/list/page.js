19f8581be2fc06ccfc14091d363d7c0770371262
Dasom Park <dev.somda@gmail.com>
2023-09-25 22:57:25 +0900

    상품 목록 표기

--- app/list/page.js
+++ app/list/page.js
@@ -1,7 +1,16 @@
 export default function List() {
+  let products = ["딸기", "블루베리", "샤인머스캣", "블랙사파이어"];
+
   return (
     <div>
       <h3>Product list</h3>
+      <div>
+        {products.map((product, idx) => (
+          <div key={idx} className="product-card">
+            <p>{product} $40</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
